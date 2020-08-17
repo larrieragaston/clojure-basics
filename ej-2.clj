60a94348005d98de677533f5310a167eaea1ebe1
Gaston Larriera <glarriera@wormholeit.com>
2020-08-17 18:33:03 -0300

    ej-2

---
+++ ej-2.clj
@@ -0,0 +1,16 @@
+;; 2. Definir la función segundos que reciba los cuatro valores (días, horas, minutos y segundos) del tiempo que dura un evento y devuelva el valor de ese tiempo expresado solamente en segundos.
+;; Sin sobrecarga
+(defn segundos [d h m s] (+ (* d 24 60 60) (* h 60 60) (* m 60) s))
+
+(defn segundos_sobrecarga 
+  ([segs] segs)
+  ([mins, segs] (segundos_sobrecarga (+ segs (* mins 60))))
+  ([hours, mins, segs] (segundos_sobrecarga (+ (* hours 60) mins) segs))
+  ([days, hours, mins, segs]  (segundos_sobrecarga (+ (* days 24) hours) mins segs))) 
+
+;; Test case
+(println (segundos 1 2 3 17))
+(println (segundos_sobrecarga 17))
+(println (segundos_sobrecarga 3 17))
+(println (segundos_sobrecarga 2 3 17))
+(println (segundos_sobrecarga 1 2 3 17))
